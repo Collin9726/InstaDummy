24d1204c63e58b4334d4b9b0f7ec75b2c67a82ee
collin9726 <owino.collin@gmail.com>
2020-07-04 13:21:03 +0300

    add NewProfileForm and create_profile view

--- instaapp/views.py
+++ instaapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .email import send_signup_email
+from .forms import NewProfileForm
 
 # Create your views here.
 def welcome(request):      
@@ -13,7 +14,7 @@ def welcome(request):
 def home(request):
 
     return render(request, 'home-page.html')
-    
+
 
 @login_required(login_url='/accounts/login/')
 def send_email(request):
@@ -22,3 +23,19 @@ def send_email(request):
     name = current_user.username
     send_signup_email(name, email)
     return HttpResponseRedirect('/')
+
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.account_holder = current_user
+            profile.save()
+        return redirect('my-profile')
+
+    else:
+        form = NewProfileForm()
+    return render(request, 'create_profile.html', {"form": form})
