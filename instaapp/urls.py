bc27f53ce4031619eb52b2d8b5a05831569adda2
collin9726 <owino.collin@gmail.com>
2020-07-04 14:23:58 +0300

    enable user to create and view their profile upon sign up

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns=[
     url(r'^$',views.welcome,name = 'welcome'),
     url(r'^home/$',views.home,name = 'home'),
     url(r'^sendemail/$',views.send_email,name = 'send-email'),
+    url(r'^createprofile/$',views.create_profile,name = 'create-profile'),
+    url(r'^myprofile/$',views.my_profile,name = 'my-profile'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- instaapp/views.py
+++ instaapp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 from .email import send_signup_email
 from .forms import NewProfileForm
+from .models import Profile, Image, Comment
 
 # Create your views here.
 def welcome(request):      
@@ -22,7 +24,7 @@ def send_email(request):
     email = current_user.email
     name = current_user.username
     send_signup_email(name, email)
-    return HttpResponseRedirect('/')
+    return redirect(create_profile)
 
 
 @login_required(login_url='/accounts/login/')
@@ -34,8 +36,22 @@ def create_profile(request):
             profile = form.save(commit=False)
             profile.account_holder = current_user
             profile.save()
-        return redirect('my-profile')
+        return redirect(my_profile)
 
     else:
         form = NewProfileForm()
-    return render(request, 'create_profile.html', {"form": form})
+    return render(request, 'create-profile.html', {"form": form})
+
+
+
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user = request.user
+    try:
+        profile = Profile.objects.get(account_holder = current_user)
+    except Profile.DoesNotExist:
+        raise Http404()
+
+    return render(request, 'my-profile.html', {"profile": profile})
+    
+
