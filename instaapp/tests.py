e2c1f9de9a512bdf53677d3c7fa6f7c799f47b67
collin9726 <owino.collin@gmail.com>
2020-07-06 15:54:17 +0300

    run tests for Image model

--- instaapp/tests.py
+++ instaapp/tests.py
@@ -12,6 +12,7 @@ class ProfileTestClass(TestCase):
 
     def tearDown(self):
         Profile.objects.all().delete()
+        User.objects.all().delete()
 
     # Testing  instance
     def test_instance(self):
@@ -44,3 +45,46 @@ class ProfileTestClass(TestCase):
         self.assertEqual(updated_profile.bio, 'otherBio')
     
 
+class ImageTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.this_user =User.objects.create_user('mimi', 'mimi@gmail.com', 'moringa')        
+        self.this_profile = Profile(bio='bio', account_holder= self.this_user)
+        self.this_profile.save_profile()
+        self.this_image = Image(image_name='myimage', caption='mycaption', profile = self.this_profile)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
+        User.objects.all().delete()
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.this_image,Image))
+
+
+    # Testing Save Method
+    def test_save_image_method(self):
+        self.this_image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    # Testing Delete Method
+    def test_delete_image_method(self):
+        self.this_image.save_image()
+        image = Image.objects.get(caption ='mycaption')
+        image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    # Testing Update Method
+    def test_update_caption_method(self):        
+        self.this_image.save_image()
+        image = Image.objects.get(caption ='mycaption')        
+        image.image_name = 'otherName'
+        image.update_caption()
+        query_set = Image.objects.all()[:1]
+        updated_image=None
+        for img in query_set:
+            updated_image = img
+        self.assertEqual(updated_image.image_name, 'otherName')
